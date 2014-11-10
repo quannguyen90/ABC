2c041d2a2281727bff6d8ff90d0d0ad66dc18710
Quan Nguyen Van <quannv.tm@gmail.com>
2014-11-10 18:27:54 +0700

    initial

---
+++ Example/Pods/Target Support Files/Pods-ABC/Pods-ABC-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// ABC
+#define COCOAPODS_POD_AVAILABLE_ABC
+#define COCOAPODS_VERSION_MAJOR_ABC 0
+#define COCOAPODS_VERSION_MINOR_ABC 1
+#define COCOAPODS_VERSION_PATCH_ABC 0
+
